70f7f30647905edfbaa7f6154248c4396797602f
Ahmed Shaikh <42516500+ahmd-sh@users.noreply.github.com>
2023-12-17 19:07:51 -0500

    optimize a bit

--- car_fleet.py
+++ car_fleet.py
@@ -4,9 +4,10 @@ def car_fleet():
     speed = [2,4,1,1,3]
 
     pair = [[p, s] for p,s in zip(position, speed)]
+    pair.sort(reverse=True, key=lambda x: x[0])
     stack = []
 
-    for p,s in sorted(pair)[::-1]:
+    for p,s in pair:
         stack.append((target-p) / s)
         if len(stack) > 1 and stack[-1] <= stack[-2]:
             stack.pop()
