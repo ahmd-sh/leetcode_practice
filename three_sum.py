b4a84dc58241fb03a3a78f50aecc358df3abeb36
Ahmed Shaikh <42516500+ahmd-sh@users.noreply.github.com>
2024-03-14 17:12:06 -0600

    minor readability improvements

--- three_sum.py
+++ three_sum.py
@@ -1,29 +1,33 @@
 def three_sum():
     nums = [-1,0,1,2,-1,-4]
 
-    l = len(nums)
-    if l < 3:
+    if len(nums) < 3:
         return []
 
     res = []
     nums.sort()
 
     for i, val in enumerate(nums):
-        if i > 0 and val == nums[i-1]:
+        if i > 0 and nums[i] == nums[i-1]:
             continue
 
-        j, k = i+1, l-1
-        while j < k:
-            ts = val + nums[j] + nums[k]
-            if ts > 0:
-                k -= 1
-            elif ts < 0:
-                j += 1
+        l, r = i+1, len(nums)-1
+        while l < r:
+            three_sum = val + nums[l] + nums[r]
+
+            if three_sum < 0:
+                l += 1
+            
+            elif three_sum > 0:
+                r -= 1
+            
             else:
-                res.append([val, nums[j], nums[k]])
-                j += 1
-                while nums[j] == nums[j-1] and j < k:
-                    j += 1
+                res.append([val, nums[l], nums[r]])
+                l += 1
+                r -= 1
+
+                while l < r and nums[l] == nums[l-1]:
+                    l += 1
 
     return res
 
