c2cf5eca5c5d213bfa6b3c307f4c1d374b6454ee
Ahmed Shaikh <42516500+ahmd-sh@users.noreply.github.com>
2024-03-19 19:40:53 -0600

    add iterative way

--- binary_search.py
+++ binary_search.py
@@ -2,18 +2,33 @@ def search():
     nums = [-1,0,3,5,9,12]
     target = 9
 
-    def bs(nums, target, l, u):
-        if l > u:
-            return -1
+    # Recursive
+    # def bs(nums, target, l, u):
+    #     if l > u:
+    #         return -1
 
-        mid = l + ((u-l)//2)
-        if target == nums[mid]:
-            return mid
-        elif target < nums[mid]:
-            return bs(nums, target, l, mid-1)
+    #     mid = l + ((u-l)//2)
+    #     if target == nums[mid]:
+    #         return mid
+    #     elif target < nums[mid]:
+    #         return bs(nums, target, l, mid-1)
+    #     else:
+    #         return bs(nums, target, mid+1, u)
+
+    # return bs(nums, target, 0, len(nums)-1)
+
+    # Iterative
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = l + (r - l) // 2
+        if target > nums[m]:
+            l = m + 1
+        elif target < nums[m]:
+            r = m - 1
         else:
-            return bs(nums, target, mid+1, u)
+            return m
 
-    return bs(nums, target, 0, len(nums)-1)
+    return -1
 
 print(search())
