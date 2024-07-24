71347c1c860e2625d0a36ac56b2f2aaafe0d6d80
Ahmed Shaikh <42516500+ahmd-sh@users.noreply.github.com>
2024-07-24 14:36:48 -0230

    using both min and max heaps

---
+++ MedianFinder.py
@@ -0,0 +1,39 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small, self.large = [], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1*num)
+
+        # Ordering
+        if (self.small and
+            self.large and
+            (-1*self.small[0]) > self.large[0]):
+            val = -1*heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        # Sizing
+        if len(self.small) > len(self.large):
+            val = -1*heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        if len(self.large) > len(self.small):
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1*val)
+
+    def findMedian(self) -> float:
+
+        if len(self.small) > len(self.large):
+            return -1*self.small[0]
+
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        
+        return ( -1*self.small[0] + self.large[0] ) / 2.0
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
