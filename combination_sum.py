176baa5d6aa51c237370f71e87799ad3b00977a9
Ahmed Shaikh <ahmed.shaikh626@gmail.com>
2024-08-01 21:59:12 -0230

    backtracking dfs with dups

--- combination_sum.py
+++ combination_sum.py
@@ -3,18 +3,18 @@
 def combination_sum(self, candidates: List[int], target: int) -> List[List[int]]:
     res = []
 
-    def dfs(i, cur, tot):
-        if tot == target:
+    def dfs(i, cur, target):
+        if target == 0:
             res.append(cur.copy())
             return
-        if i >= len(candidates) or tot > target:
+        if i >= len(candidates) or target <= 0:
             return
 
         cur.append(candidates[i])
-        dfs(i, cur, tot + candidates[i])
+        dfs(i, cur, target - candidates[i])
 
         cur.pop()
-        dfs(i + 1, cur, tot)
+        dfs(i + 1, cur, target)
 
-    dfs(0, [], 0)
+    dfs(0, [], target)
     return res
